120f28d4dbe5b05b54b6608eeb762cfc8bc84a97
supreet29 <32635530+supreet29@users.noreply.github.com>
2017-12-01 02:02:49 +0000

    Adding a function

--- fileserver.py
+++ fileserver.py
@@ -12,43 +12,51 @@ import utils
 class FileServer:
     """File Server is responsible for holding and sharing file"""
 
-def GET(self, filepath):
-    """Return the file if it's not locked"""
+    def GET(self, filepath):
+        """Return the file if it's not locked"""
     
-    _raise_if_locked(filepath)
-    _raise_if_not_exists(filepath)
-    _raise_if_dir_or_not_servable(filepath)
+        _raise_if_locked(filepath)
+        _raise_if_not_exists(filepath)
+        _raise_if_dir_or_not_servable(filepath)
     
     
-    p = _get_local_path(filepath)
-    with open(p) as f:
+        p = _get_local_path(filepath)
+        with open(p) as f:
         return f.read()
 
-def DELETE(self, filepath):
-    """Remove the filepath if it's not locked, or if the correct
+    def DELETE(self, filepath):
+        """Remove the filepath if it's not locked, or if the correct
         lock_id matches in 'lock_id'."""
     
-    _raise_if_locked(filepath)
-    _raise_if_not_exists(filepath)
-    _raise_if_dir_or_not_servable(filepath)
+        _raise_if_locked(filepath)
+        _raise_if_not_exists(filepath)
+        _raise_if_dir_or_not_servable(filepath)
     
     
-    os.unlink(_get_local_path(filepath))
-    return 'OK'
+        os.unlink(_get_local_path(filepath))
+        return 'OK'
 
-def PUT(self, filepath):
-    """Replace the file."""
+    def PUT(self, filepath):
+        """Replace the file."""
         
-    _raise_if_locked(filepath)
-    _raise_if_dir_or_not_servable(filepath)
+        _raise_if_locked(filepath)
+        _raise_if_dir_or_not_servable(filepath)
         
         p = _get_local_path(filepath)
         
         with open(p, 'w') as f:
             f.write(web.data())
         
-    return ''
+        return ''
 
+    def HEAD(self, filepath):
+        
+        _raise_if_locked(filepath)
+        _raise_if_not_exists(filepath)
+        _raise_if_dir_or_not_servable(filepath)
 
+        p = _get_local_path(filepath)
+        
+        return ''
 
 
