3a6667b8360d4351a388feec848bdaa595360c4f
supreet29 <32635530+supreet29@users.noreply.github.com>
2017-12-03 04:14:08 +0000

    changes

--- fileserver.py
+++ fileserver.py
@@ -4,6 +4,7 @@ import logging
 import utils
 from contextlib import closing
 import http.client
+import time
 
 urls = (
         '/filepath/(.*)', 'Fileserver'
@@ -12,67 +13,36 @@ urls = (
 class Fileserver:
     def GET(self,filepath):
         # To open and read the requested file (filepath is passed).
-        i = web.input()
         
-        host, port = utils.get_host_port(_config['lockserver'])
-        if utils.is_locked(filepath, host, port, i.get('lock_id', None)):
-            raise web.unauthorized()
-    
-        
-        p = os.path.join(os.getcwd(), _config['fsroot'], filepath[1:])
-        
-        if (os.path.dirname(filepath) not in _config['directories'] or
-            os.path.isdir(p)):
-            # request a file which this server isn't supposed to serve!
-            raise web.notacceptable()
-            
-            if not os.path.exists(p):
-                raise web.webapi.HTTPError('204 No Content',
-                                           {'Content-Type': 'plain/text'})
-                    
-                    with open(p) as f:
-                        return f.read()
+        #p = os.path.isfile(filepath)
+        print("inside get")
+        if os.path.isfile(filepath):
+            with open(filepath) as f:
+                return f.read()
+        else:
+            return "wrong path"
 
 def PUT(self, filepath):
     """Replace the file by the data in the request."""
-        i = web.input()
         
-        host, port = utils.get_host_port(_config['lockserver'])
-        if utils.is_locked(filepath, host, port, i.get('lock_id', None)):
-            raise web.unauthorized()
-    
-    
-        p = os.path.join(os.getcwd(), _config['fsroot'], filepath[1:])
+        p = os.path.isfile(filepath)
         
-        if (os.path.dirname(filepath) not in _config['directories'] or
-            os.path.isdir(p)):
-            # request a file which this server isn't supposed to serve!
-            raise web.notacceptable()
-            
-            if not os.path.exists(p):
-                raise web.webapi.HTTPError('204 No Content',
-                                           {'Content-Type': 'plain/text'})
-                    
-                    with open(p, 'w') as f:
-                        f.write(web.data())
-                            
-                            return ''
+        with open(filepath, 'w') as f:
+            print(web.data())
+            f.write(web.data().decode())
 
-_config = {
-    'lockserver': None,
-        'nameserver': None,
-        'directories': [],
-        'fsroot': 'fs/',
-        'srv': None,
-    }
+    return ''
 
-logging.info('Loading config file fileserver.dfs.json.')
-utils.load_config(_config, 'fileserver.dfs.json')
+def DELETE(self, filepath):
+    os.unlink(_get_local_path(filepath))
+    return 'OK'
 
-# just to speed up the search to know if we can serve a file
-# O(n) → O(log n)
-_config['directories'] = set(_config['directories'])
+def HEAD(self, filepath):
+    p = _get_local_path(filepath)
+        web.header('Last-Modified', time.ctime(os.path.getmtime(p)))
+        return ''
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
     app.run()
+
