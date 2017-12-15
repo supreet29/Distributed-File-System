776a9db38a21028afe8b50db56baa7eacd87bd10
Supreet Singh <supreetsingh@Supreets-MacBook-Air.local>
2017-12-15 09:31:17 +0000

    adding basic funtaionality client

---
+++ client1.py
@@ -0,0 +1,32 @@
+import requests as req
+import json
+import shelve
+from requests.auth import HTTPBasicAuth
+import os
+
+url = "http://0.0.0.0:8080/filepath/"
+print("==================================================================")
+print("\t\tWelcome to Distributed File System")
+print("==================================================================\n")
+print("1. Read File")
+print("2. Write File")
+print("3. Delete File\n")
+options= input("Select the option to:- \n")
+filepath = input("Enter the file name:")
+if options=='1':
+    filepath = input("Enter the file name:")
+    url = url+filepath
+    response = req.get(url)
+    print("Response: ",response.text)
+if options=='2':
+    filepath = input("Enter the file name:")
+    url = url+filepath
+    content = "hello"
+    response= req.post(url, content)
+    print("Response: ",response.text)
+if options=='3':
+    filepath = input("Enter the file name:")
+    url = url+filepath
+    response = req.delete(url)
+    print("File removed")
+
